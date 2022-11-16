7983de99f14d5fb4a56b2bd5a1da444266158005
pscommune <daniil.mukhametov.96@mail.ru>
2022-11-16 14:17:49 +0500

    3 commit

---
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.criminalintent
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+
+  private val crimeRepository= CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+  fun addCrime(crime: Crime) {
+    crimeRepository.addCrime(crime)
+  }
+}
